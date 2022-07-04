61b744631412823866f6d649fc7a095e1c6d3bd5
Leandro Silva <leanoaks@gmail.com>
2022-07-04 20:17:10 -0300

    defined basic structs and enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+enum State {
+    Water,
+    Ship,
+    Destroyed,
+}
+
+
+struct Cell {
+    state :State,
+}
+
+struct Grid {
+    cells :[[Cell; 9]; 9],
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
