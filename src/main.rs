176b3655857df7862806062e416e1ab48eab3367
Leandro Silva <leanoaks@gmail.com>
2022-07-04 22:20:45 -0300

    Cell and State were fused in just one enum called Cell, added implementations for Grid and Cell

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,61 @@
-enum State {
+
+#[derive(Clone)]
+
+enum Cell {
     Water,
     Ship,
     Destroyed,
 }
 
-
-struct Cell {
-    state :State,
+impl Cell {
+    fn chr_sprite(&self) -> char {
+        match self {
+            Cell::Water => '.',
+            Cell::Ship => '@',
+            Cell::Destroyed => '*',
+        }
+    }
 }
 
+
 struct Grid {
-    cells :[[Cell; 9]; 9],
+    cells :Vec<Vec<Cell>>,
+}
+
+impl Grid {
+    fn print(&self) {
+        let mut row_counter = 0;
+        for row in &self.cells {
+            for cell in row {
+                print!(" {}", cell.chr_sprite());
+            }
+            println!(" {}", row_counter);
+            row_counter += 1;
+        }
+        println!(" A B C D E F G H I");
+    }
+
+    fn new_of_size(size :u8) -> Grid {
+        let cell :Cell = Cell::Water;
+        let mut row :Vec<Cell> = Vec::new();
+        let mut column :Vec<Vec<Cell>> = Vec::new();
+
+        for _i in 0..size {
+            row.clear();
+            for _j in 0..size {
+                row.push(cell.clone());
+            }
+            column.push(row.clone());
+        }
+        Grid {
+            cells: column,
+        }
+    }
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let grid :Grid = Grid::new_of_size(12);
+    grid.print();
 }
+
